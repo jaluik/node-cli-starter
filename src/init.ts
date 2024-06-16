2c6d4676225507ddaae65fbab83c31bedcf99ac1
jaluik <cqzhengmouren@gmail.com>
2024-06-16 23:34:59 +0800

    feat: 完善写入逻辑

--- src/init.ts
+++ src/init.ts
@@ -5,6 +5,8 @@ import {
   detectPackageManager,
   installJest,
   writeJestConfig,
+  installAllDependencies,
+  updatePackageJsonScripts,
 } from './utils';
 import sharedVars from './sharedVars';
 
@@ -15,25 +17,34 @@ async function init(path: string) {
   const spinner = ora('分析项目中...').start();
 
   try {
-    const packageJson = getPackageJson();
+    const packageJson = await getPackageJson();
+    spinner.info('安装依赖...');
+
+    await installAllDependencies();
+    spinner.succeed('安装依赖成功。');
 
     spinner.text = '检测技术栈...';
-    const techStack = detectTechStack(packageJson);
+    const techStack = await detectTechStack(packageJson);
     if (techStack.techStack === 'unknown') {
       throw new Error('未检测到技术栈。请确保您在一个 React 或 Vue 项目中。');
     }
-    spinner.succeed(`检测到的技术栈: ${techStack}`);
+    spinner.succeed(
+      `检测到的技术栈: ${techStack.techStack}，版本为：${techStack.version}`,
+    );
 
     spinner.start('检测包管理器...');
-    const packageManager = detectPackageManager();
+    const packageManager = await detectPackageManager();
     spinner.succeed(`检测到的包管理器: ${packageManager}`);
 
-    spinner.start('安装 Jest...');
-    installJest(packageManager);
+    spinner.info('安装 Jest...');
+    await installJest(packageManager);
     spinner.succeed('Jest 安装成功。');
 
     spinner.start('写入 Jest 配置...');
-    writeJestConfig();
+    await updatePackageJsonScripts({
+      test: 'jest',
+    });
+    await writeJestConfig();
     spinner.succeed('Jest 配置写入成功。');
   } catch (error) {
     spinner.fail(`操作失败: ${(error as Error).message}`);
--- src/utils.ts
+++ src/utils.ts
@@ -1,22 +1,51 @@
 import * as fs from 'fs-extra';
 import * as path from 'path';
-import { execSync } from 'child_process';
 import { PackageJsonType, PackageManagerType, TechStackType } from './types';
 import sharedVars from './sharedVars';
 import ora from 'ora';
+import { spawn } from 'child_process';
 
 const spinner = ora();
 
 const { getBasePath } = sharedVars;
 
-export function getPackageJson(): PackageJsonType {
+const execAsync = (command: string, options = {}): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    const proc = spawn(command, {
+      shell: true,
+      stdio: 'inherit',
+      cwd: getBasePath(),
+      ...options,
+    });
+
+    let output = '';
+
+    proc.stdout?.on('data', (data) => {
+      output += data.toString();
+    });
+
+    proc.stderr?.on('data', (data) => {
+      output += data.toString();
+    });
+
+    proc.on('close', (code) => {
+      if (code === 0) {
+        resolve(output);
+      } else {
+        reject(new Error(`Command failed with exit code ${code}`));
+      }
+    });
+  });
+};
+
+export async function getPackageJson(): Promise<PackageJsonType> {
   const packageJsonPath = path.resolve(getBasePath(), 'package.json');
-  if (!fs.existsSync(packageJsonPath)) {
+  if (!(await fs.pathExists(packageJsonPath))) {
     spinner.fail('package.json not found');
     process.exit(1);
   }
 
-  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
+  const packageJson: PackageJsonType = await fs.readJSON(packageJsonPath);
   return packageJson;
 }
 
@@ -26,31 +55,35 @@ export function getPackageJson(): PackageJsonType {
  * @param packageManager 包管理工具（npm、yarn 或 pnpm）
  * @returns 包的版本号
  */
-export const getPackageVersion = (
+export const getPackageVersion = async (
   packageName: string,
   packageManager: 'npm' | 'yarn' | 'pnpm',
-): string | null => {
+): Promise<string | null> => {
   try {
     let version: string;
 
     switch (packageManager) {
       case 'npm':
-        version = execSync(
-          `npm list ${packageName} --depth=0 --json`,
-        ).toString();
+        version = await execAsync(`npm list ${packageName} --depth=0 --json`, {
+          stdio: undefined,
+        });
         return JSON.parse(version).dependencies[packageName].version;
 
       case 'yarn':
-        version = execSync(
+        version = await execAsync(
           `yarn list --pattern ${packageName} --depth=0 --json`,
-        ).toString();
+          {
+            stdio: undefined,
+          },
+        );
         const yarnData = JSON.parse(version);
         return yarnData.data.trees[0].name.split('@')[1];
 
       case 'pnpm':
-        version = execSync(
-          `pnpm list ${packageName} --depth=0 --json`,
-        ).toString();
+        version = await execAsync(`pnpm list ${packageName} --depth=0 --json`, {
+          stdio: undefined,
+        });
+
         const pnpmData = JSON.parse(version);
         return pnpmData[0].dependencies[packageName].version;
 
@@ -63,19 +96,21 @@ export const getPackageVersion = (
   }
 };
 
-export function detectPackageManager(): PackageManagerType {
-  if (fs.existsSync(path.resolve(getBasePath(), 'yarn.lock'))) {
+export async function detectPackageManager(): Promise<PackageManagerType> {
+  if (await fs.pathExists(path.resolve(getBasePath(), 'yarn.lock'))) {
     return 'yarn';
-  } else if (fs.existsSync(path.resolve(getBasePath(), 'pnpm-lock.yaml'))) {
+  } else if (
+    await fs.pathExists(path.resolve(getBasePath(), 'pnpm-lock.yaml'))
+  ) {
     return 'pnpm';
   }
   return 'npm';
 }
 
-export function detectTechStack(packageJson: PackageJsonType): {
+export async function detectTechStack(packageJson: PackageJsonType): Promise<{
   techStack: TechStackType;
   version?: string;
-} {
+}> {
   let techStack: TechStackType = 'unknown';
   if (packageJson.dependencies?.react || packageJson.devDependencies?.react) {
     techStack = 'react';
@@ -85,31 +120,45 @@ export function detectTechStack(packageJson: PackageJsonType): {
   if (techStack === 'unknown') {
     return { techStack };
   }
-  const packageManager = detectPackageManager();
-  const version = getPackageVersion('react', packageManager);
+  const packageManager = await detectPackageManager();
+  const version = await getPackageVersion('react', packageManager);
 
   return { techStack, version };
 }
 
-export function installJest(packageManager: PackageManagerType): void {
+export const installAllDependencies = async () => {
+  const packageManager = await detectPackageManager();
+  await execAsync(`${packageManager} install`, {
+    cwd: getBasePath(),
+  });
+  return;
+};
+
+export async function installJest(
+  packageManager: PackageManagerType,
+): Promise<void> {
   const installCommand = {
     npm: 'npm install --save-dev jest',
     yarn: 'yarn add --dev jest',
     pnpm: 'pnpm add --save-dev jest',
   };
 
-  execSync(installCommand[packageManager], { stdio: 'inherit' });
+  await execAsync(installCommand[packageManager], {
+    cwd: getBasePath(),
+  });
 }
 
-export function writeJestConfig(): void {
+export async function writeJestConfig(): Promise<void> {
   const jestConfigPath = path.resolve(getBasePath(), 'jest.config.js');
-  const templatePath = path.resolve(__dirname, 'jest.config.template.js');
-  fs.copyFileSync(templatePath, jestConfigPath);
+  const templatePath = path.resolve(__dirname, '../template/jest.config.js');
+  await fs.copy(templatePath, jestConfigPath);
 }
 
-export function updatePackageJsonScripts(obj: Record<string, string>) {
+export async function updatePackageJsonScripts(
+  obj: Record<string, string>,
+): Promise<void> {
   const packageJsonPath = path.resolve(getBasePath(), 'package.json');
-  const packageJson = getPackageJson();
+  const packageJson = await getPackageJson();
 
   packageJson.scripts = packageJson.scripts || {};
   packageJson.scripts = {
@@ -117,6 +166,5 @@ export function updatePackageJsonScripts(obj: Record<string, string>) {
     ...obj,
   };
 
-  fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
-  spinner.succeed('Updated package.json scripts with "test: jest"');
+  await fs.writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2));
 }
